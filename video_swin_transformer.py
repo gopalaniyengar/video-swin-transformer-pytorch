d52e368786196c9e688f15dcde2de3c84f6facca
Gopalan Iyengar <63103249+gopalaniyengar@users.noreply.github.com>
2022-03-18 19:10:14 +0530

    Add files via upload

--- video_swin_transformer.py
+++ video_swin_transformer.py
@@ -676,3 +676,132 @@ class SwinTransformer3D(nn.Module):
         """Convert the model into training mode while keep layers freezed."""
         super(SwinTransformer3D, self).train(mode)
         self._freeze_stages()
+
+class I3DHead(nn.Module):
+    """
+    Base class for head.
+    All Head should subclass it.
+    All subclass should overwrite:
+    - Methods:``init_weights``, initializing weights in some modules.
+    - Methods:``forward``, supporting to forward both for training and testing.
+    Args:
+        num_classes (int): Number of classes to be classified.
+        in_channels (int): Number of channels in input feature.
+        loss_cls (dict): Config for building loss.
+            Default: dict(type='CrossEntropyLoss', loss_weight=1.0).
+        multi_class (bool): Determines whether it is a multi-class
+            recognition task. Default: False.
+        label_smooth_eps (float): Epsilon used in label smooth.
+            Reference: arxiv.org/abs/1906.02629. Default: 0.
+    """
+    """
+    Classification head for I3D.
+    Args:
+        num_classes (int): Number of classes to be classified.
+        in_channels (int): Number of channels in input feature.
+        loss_cls (dict): Config for building loss.
+            Default: dict(type='CrossEntropyLoss')
+        spatial_type (str): Pooling type in spatial dimension. Default: 'avg'.
+        dropout_ratio (float): Probability of dropout layer. Default: 0.5.
+        init_std (float): Std value for Initiation. Default: 0.01.
+        kwargs (dict, optional): Any keyword argument to be used to initialize
+            the head.
+    """
+
+    def __init__(self,
+                 num_classes,
+                 in_channels,
+                 loss_cls=dict(type='CrossEntropyLoss'),
+                 multi_class=False,
+                 label_smooth_eps=0.0,
+                 spatial_type='avg',
+                 dropout_ratio=0.5,
+                 init_std=0.01,
+                 **kwargs):
+        super().__init__()
+        
+        self.num_classes = num_classes
+        self.in_channels = in_channels
+        self.multi_class = multi_class
+        self.label_smooth_eps = label_smooth_eps
+
+        self.spatial_type = spatial_type
+        self.dropout_ratio = dropout_ratio
+        self.init_std = init_std
+        if self.dropout_ratio != 0:
+            self.dropout = nn.Dropout(p=self.dropout_ratio)
+        else:
+            self.dropout = None
+
+        self.fc_cls = nn.Linear(self.in_channels, self.num_classes)
+
+        if self.spatial_type == 'avg':
+            # use `nn.AdaptiveAvgPool3d` to adaptively match the in_channels.
+            self.avg_pool = nn.AdaptiveAvgPool3d((1, 1, 1))
+        else:
+            self.avg_pool = None
+
+    def init_weights(self):
+        """Initiate the parameters from scratch."""
+        normal_init(self.fc_cls, std=self.init_std)
+
+    def forward(self, x):
+        """Defines the computation performed at every call.
+        Args:
+            x (torch.Tensor): The input data.
+        Returns:
+            torch.Tensor: The classification scores for input samples.
+        """
+        # [N, in_channels, 4, 7, 7]
+        if self.avg_pool is not None:
+            x = self.avg_pool(x)
+        # [N, in_channels, 1, 1, 1]
+        if self.dropout is not None:
+            x = self.dropout(x)
+        # [N, in_channels, 1, 1, 1]
+        x = x.view(x.shape[0], -1)
+        # [N, in_channels]
+        cls_score = self.fc_cls(x)
+        # [N, num_classes]
+        return cls_score
+
+class SwinT_Kinetics(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = SwinTransformer3D(embed_dim=96, 
+                          depths=[2, 2, 6, 2], 
+                          num_heads=[3, 6, 12, 24], 
+                          patch_size=(2,4,4), 
+                          window_size=(8,7,7), 
+                          patch_norm=True)
+
+        self.head = I3DHead(num_classes=400, in_channels=768)
+    
+    def forward(self, input_video):
+        features = self.backbone(input_video)
+        logits = self.head(features)
+        return logits
+
+if __name__=='__main__':
+    from torchinfo import summary
+    
+    '''
+    swint_backbone = SwinTransformer3D(embed_dim=96, 
+                          depths=[2, 2, 6, 2], 
+                          num_heads=[3, 6, 12, 24], 
+                          patch_size=(2,4,4), 
+                          window_size=(8,7,7), 
+                          patch_norm=True)
+    summary(swint_backbone)
+
+    i3d = I3DHead(num_classes=400, in_channels=768)
+    summary(i3d)
+    '''
+    
+    swint_classifier = SwinT_Kinetics()
+    dummy = torch.rand(1, 3, 32, 224, 224)
+    print(dummy.shape)
+    preds = swint_classifier(dummy)
+    print(preds.shape)
+    summary(swint_classifier, input_data=dummy, col_names=["input_size", "output_size", "num_params"])#, "kernel_size", "mult_adds"])))
+
